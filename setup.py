3e445aefa19e780dc8ae0363ab7027b5af2f838a
Sejun Kim <sejun.kim@intel.com>
2022-12-01 19:10:31 -0800

    Refactoring

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ class CMakeBuild(build_ext):
             build_args += ["--", "/m"]
         else:
             cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-            cpu_count = 1 if os.cpu_count() <= 1 else os.cpu_count() - 1
+            cpu_count = os.cpu_count() - 1 if os.cpu_count() > 1 else 1
             build_args += ["--", "-j" + str(cpu_count)]
 
         env = os.environ.copy()
